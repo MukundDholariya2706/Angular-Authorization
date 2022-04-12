4d56637b849125eb720e9d1311b0f0f9a13a244f
Mukund <mukunddtridhyatech@gmail.com>
2022-04-12 10:50:58 +0530

    create components, raouting & auth-guard

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { Component1Component } from './components/component1/component1.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Component2Component } from './components/component2/component2.component';
+
+const routes: Routes = [
+  {
+    path: 'component1',
+    component: Component1Component
+  },
+  {
+    path: 'component2',
+    component: Component2Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
